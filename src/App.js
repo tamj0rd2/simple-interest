a91daa00ae92cbfab4e73ca2c2f497b9ea6bdb3a
tamj0rd2 <tamara.jordan1+coding@hotmail.com>
2017-07-07 00:57:47 +0100

    Apply codestyle

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        Hello world!
-      </div>
-    );
+  render () {
+    return <div>Hello world!</div>
   }
 }
 
-export default App;
+export default App
