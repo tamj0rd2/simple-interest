b8ba99b3d4381097c846666f471f6f5bece3c894
tamj0rd2 <tamara.jordan1+coding@hotmail.com>
2017-07-07 12:38:06 +0100

    Add tests for Results

---
+++ src/components/Results.test.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Results from './Results'
+
+import initialState from '../constants/initialState'
+import { shallow } from 'enzyme'
+import { expect } from 'chai'
+
+const props = {
+  earnedPA: 240,
+  selectedCurrency: Object.assign({}, initialState.currencies[0])
+}
+
+describe('Output', () => {
+  const wrapper = shallow(<Results {...props} />)
+
+  it('contains a CurrencyGroup that shows the amount earned per month', () => {
+    let group = wrapper.find('CurrencyGroup[scope="month"]')
+    expect(group).to.have.length(1)
+    expect(group.prop('value')).to.equal(20)
+    expect(group.prop('exchangeRate')).to.equal(1)
+    expect(group.prop('currencySymbol')).to.equal('£')
+  })
+
+  it('contains a CurrencyGroup that shows the amount earned per year', () => {
+    let group = wrapper.find('CurrencyGroup[scope="year"]')
+    expect(group).to.have.length(1)
+    expect(group.prop('value')).to.equal(240)
+    expect(group.prop('exchangeRate')).to.equal(1)
+    expect(group.prop('currencySymbol')).to.equal('£')
+  })
+})
